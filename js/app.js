13f5d7101ffc0bd611294565fd54e8bfa0d454d5
othree <othree@gmail.com>
2012-07-27 19:25:03 +0800

    Add new files, legacy folder

---
+++ js/app.js
@@ -0,0 +1,4 @@
+define(['vender/jquery-1.7.2.min', 'vender/underscore-min'], function () {
+
+    return {};
+});
---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+require(['app', 'vender/jquery-1.7.2.min', 'vender/underscore-min'], function (app) {
+
+});

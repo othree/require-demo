053ad4101a4bffde4afb9e3c83e96b975f9667fa
othree <othree@gmail.com>
2012-07-29 19:29:55 +0800

    Update legacy codes

--- legacy/one/js/data.js
+++ legacy/one/js/data.js
@@ -1,4 +1,6 @@
 
+// (function(){function r(a,c,d){if(a===c)return 0!==a||1/a==1/c;if(null==a||null==c)return a===c;a._chain&&(a=a._wrapped);c._chain&&(c=c._wrapped);if(a.isEqual&&b.isFunction(a.isEqual))return a.isEqual(c);if(c.isEqual&&b.isFunction(c.isEqual))return c.isEqual(a);var e=l.call(a);if(e!=l.call(c))return!1;switch(e){case "[object String]":return a==""+c;case "[object Number]":return a!=+a?c!=+c:0==a?1/a==1/c:a==+c;case "[object Date]":case "[object Boolean]":return+a==+c;case "[object RegExp]":return a.source==
+
 var data = {
     init: function () {
         if (account && device) {
